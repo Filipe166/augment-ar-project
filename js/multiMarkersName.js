933f18099c9e5c5fdd20888d546f507160267d43
Filipe Campos <filipe.campos166@gmail.com>
2022-06-03 11:45:35 +0200

    chore: add screen lock orientation and new marker

--- js/multiMarkersName.js
+++ js/multiMarkersName.js
@@ -66,3 +66,8 @@ AFRAME.registerComponent("registerevents", {
     });
   },
 });
+
+screen.addEventListener("orientationchange", function () {
+  console.log("The orientation of the screen is: " + screen.orientation);
+});
+screen.lockOrientation("landscape");
