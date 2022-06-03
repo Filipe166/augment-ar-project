947bf31ed6131d553711a7ef37f8303d77c67b9e
Filipe Campos <filipe.campos166@gmail.com>
2022-06-03 10:30:37 +0200

    fix: possible fix for samsung internet zoom bug

--- js/multiMarkersName.js
+++ js/multiMarkersName.js
@@ -63,34 +63,3 @@ AFRAME.registerComponent("registerevents", {
     });
   },
 });
-
-const toggleFullScreen = () => {
-  console.log("make full screen");
-
-  var doc = window.document;
-  var docEl = doc.documentElement;
-
-  var requestFullScreen =
-    docEl.requestFullscreen ||
-    docEl.mozRequestFullScreen ||
-    docEl.webkitRequestFullScreen ||
-    docEl.msRequestFullscreen;
-  var cancelFullScreen =
-    doc.exitFullscreen ||
-    doc.mozCancelFullScreen ||
-    doc.webkitExitFullscreen ||
-    doc.msExitFullscreen;
-
-  if (
-    !doc.fullscreenElement &&
-    !doc.mozFullScreenElement &&
-    !doc.webkitFullscreenElement &&
-    !doc.msFullscreenElement
-  ) {
-    requestFullScreen.call(docEl);
-  } else {
-    cancelFullScreen.call(doc);
-  }
-};
-
-toggleFullScreen();
